19c79865e81db64ad08b9f6d88fb31e071f51dae
Ruan Barreto <ruanrock@hotmail.com>
2024-05-27 21:53:14 -0300

    new file_add1

--- Trab_Files_Path_Abs_Relativo.py
+++ Trab_Files_Path_Abs_Relativo.py
@@ -6,3 +6,12 @@ print(Path.cwd())
 print(Path.cwd().is_absolute())
 
 print(os.path.abspath('.'))
+
+# Extrair partes do arquivo
+p = Path("c:/aulas/python/scripts.py")
+print(p.anchor)
+print(p.drive)
+print(p.parent)
+print(p.stem)
+print(p.suffix)
+print(p.parents)

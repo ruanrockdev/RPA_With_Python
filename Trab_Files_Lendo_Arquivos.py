d836363813dca606bbd4d4bfa5463ced0950b48b
Ruan Barreto <ruanrock@hotmail.com>
2024-05-29 20:48:33 -0300

    add soneto.txt e Lendo_Arquivos

---
+++ Trab_Files_Lendo_Arquivos.py
@@ -0,0 +1,17 @@
+# Lendo arquivos com Python
+
+# Estou lendo apenas o arquivo por que ele está na mesma pasta que estou trabalhando
+# Passamos o encoding para aceitar os acentos
+with open("soneto.txt", encoding="utf8") as f:
+    contents = f.read()
+    print(contents)
+
+# Vamos contar as linhas usando um contador
+lines = []
+with open("soneto.txt", encoding="utf8") as f:
+    lines = f.readlines()
+
+count = 0
+for line in lines:
+    count += 1
+    print(f"line {count}: {line}")

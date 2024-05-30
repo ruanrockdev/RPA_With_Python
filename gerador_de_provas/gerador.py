3f0d13a1d140b680d2d829ac16a6b432fc61850e
Ruan Barreto <ruanrock@hotmail.com>
2024-05-29 21:54:22 -0300

    att gerador.py

---
+++ gerador_de_provas/gerador.py
@@ -0,0 +1,36 @@
+# Criando um gerador de provas com questões em ordem aleatória
+
+import random
+
+capitais = {
+    'Acre': 'Rio Branco',
+    'Alagoas': 'Maceió',
+    'Amapá': 'Macapá',
+    'Amazonas': 'Manaus',
+    'Bahia': 'Salvador',
+    'Ceará': 'Fortaleza',
+    'Espírito Santo': 'Vitória',
+    'Goiás': 'Goiânia',
+    'Maranhão': 'São Luís',
+    'Mato Grosso': 'Cuiabá',
+    'Mato Grosso do Sul': 'Campo Grande',
+    'Minas Gerais': 'Belo Horizonte',
+    'Pará': 'Belém',
+    'Paraíba': 'João Pessoa',
+    'Paraná': 'Curitiba',
+    'Pernambuco': 'Recife',
+    'Piauí': 'Teresina',
+    'Rio de Janeiro': 'Rio de Janeiro',
+    'Rio Grande do Norte': 'Natal',
+    'Rio Grande do Sul': 'Porto Alegre',
+    'Rondônia': 'Porto Velho',
+    'Roraima': 'Boa Vista',
+    'Santa Catarina': 'Florianópolis',
+    'São Paulo': 'São Paulo',
+    'Sergipe': 'Aracaju',
+    'Tocantins': 'Palmas',
+    'Distrito Federal': 'Brasília'
+}
+
+for i in capitais.keys():
+    print(f"Chave:{i}, Valor:{capitais[i]}")

9054b1e2a6b7d5a70b0358074792b460566e2dc6
Ruan Barreto <ruanrock@hotmail.com>
2024-05-29 21:00:42 -0300

    Add_File_Escrev_Text_Files

---
+++ Trab_Files_Escrev_Text_Files.py
@@ -0,0 +1,9 @@
+# Criando arquivo com texto dentro
+
+lines = ["readme", "How to write text files in Python"]
+with open("readme.txt", "w") as f:
+    for line in lines:
+        f.write(line)
+        f.write("\n")
+
+print("-----------------------------------------------")

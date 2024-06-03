2662ca8b8803c1c6387307035bb8e67cc85fc9bc
Ruan Barreto <ruanrock@hotmail.com>
2024-06-03 20:31:45 -0300

    add1

--- PythonFiles_for_Tests/Excel_Conv_Plan_Json.py
+++ PythonFiles_for_Tests/Excel_Conv_Plan_Json.py
@@ -1,1 +1,11 @@
 # Vamos converter xlsx in Json and more, lets do it ;)
+import openpyxl
+
+wb = openpyxl.load_workbook("products.xlsx")
+sheet = wb["Sheet1"]
+
+# Obtendo o total de linhas com dados para usar
+nTotalLinhas = sheet.max_row
+
+for row in sheet.iter_rows(min_row=1, max_row=nTotalLinhas, min_col=1, max_col=3, values_only=True):
+    print(row)
